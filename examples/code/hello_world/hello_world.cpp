c8dac56f19d78306cc41cca1a8dbc71cb7eff644
Christopher Skeen <skeen@email.sc.edu>
2017-01-12 15:36:32 -0500

    Hello World Example

---
+++ examples/code/hello_world/hello_world.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!\n";
+	return 0;
+}
+
+// Thoughts about this example...
+//
+// How is 'int main() {' different from its java equivalent 'public static void main() {'?
+//
+// What is 'using namespace std;' doing in this code? Explain it in your own words, to your own satisfaction.
+//
+// Explain in your own words why 'cout << "Hello World!\n"' is structured like it is.
+//
+// Would 'std.cout << "Hello World!\n";' work just as well?
