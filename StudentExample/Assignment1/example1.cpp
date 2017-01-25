c503f034c7a678ff858ec356dc87f93509153f88
Kendrick Dubose <kendrickdubose@MacBook-Pro.local>
2017-01-24 20:41:34 -0500

    Working on properly taking input.

---
+++ StudentExample/Assignment1/example1.cpp
@@ -0,0 +1,17 @@
+// Kendrick Dubose, 1/24/17 kdd1@email.sc.edu 
+
+#include <iostream>
+using namespace std; 
+int main(int argc, char **argv)
+{
+	//Should take in 5 int 
+	char a = ' '; 
+	cout << "Please insert 5 characters, and they will be printed out in the form of a box." << endl;
+	
+	cin >> a ; 
+	
+	cout << a; 
+	
+	return 0;
+}
+
