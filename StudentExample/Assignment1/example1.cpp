0c2b78164dd2a07e5adfe7e17b8a12554afdb8f6
Kendrick Dubose <kendrickdubose@MacBook-Pro.local>
2017-01-24 21:02:26 -0500

    print out chars inputted in the array and output.

--- StudentExample/Assignment1/example1.cpp
+++ StudentExample/Assignment1/example1.cpp
@@ -5,13 +5,20 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	//Should take in 5 int 
-	char a = ' '; 
+	char * P; //created a pointer P 
+	P = new char[5]; //created an array inside of that pointer
 	cout << "Please insert 5 characters, and they will be printed out in the form of a box." << endl;
 	
-	cin >> a ; 
-	
-	cout << a; 
-	
+	cin >> P ;
+    for( int i = 0; i < 5; i++)
+    {	
+       cout << P[i]<< P[i]<< P[i] << P[i] << P[i] << endl;
+       
+    }
+    
+    delete P;
+	     
+	 
 	return 0;
 }
 
