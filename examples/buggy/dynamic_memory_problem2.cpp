edf02e63b8d648fb8b654db897410e856f8d846b
Christopher Skeen <skeen@email.sc.edu>
2017-01-29 16:06:45 -0500

    Buggy Code Examples

---
+++ examples/buggy/dynamic_memory_problem2.cpp
@@ -0,0 +1,11 @@
+void main() {
+	int integer1 = new int;
+	int integer2 = new int;
+	int integer3 = new int;
+	int * output;
+	output = new int;
+	integer1 = 5;
+	integer2 = 3;
+	integer3 = 10;
+	output = integer1 + integer2 + integer3;
+}
