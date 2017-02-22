2a149236561bceb94b6206f8f13ad6eef4de3009
Chris Skeen <skeen.c@gmail.com>
2017-02-21 20:44:40 -0500

    Adding Partial Solution

---
+++ examples/code/linked_string/solution/main.cpp
@@ -0,0 +1,12 @@
+#include "simple_string.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// The function of this program is to insert string2 into string1 between "and" and "years"
+	// then concatinate the new string1 and string3, then print them to the console.
+	Simple_String string1("Four score and years ago,");
+	Simple_String string2("seven ");
+	Simple_String string3(" our fathers brought forth on this continent a new nation...");
+}
