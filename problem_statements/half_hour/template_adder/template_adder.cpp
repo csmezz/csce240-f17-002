87ed382180fdb10d13dc1b37d64b1c9cfbf63723
Chris Skeen <skeen.c@gmail.com>
2017-04-04 15:27:58 -0400

    Final update of template_adder

--- problem_statements/half_hour/template_adder/template_adder.cpp
+++ problem_statements/half_hour/template_adder/template_adder.cpp
@@ -3,14 +3,13 @@
 
 using namespace std;
 
-Number * first;
-Number * second;
-
 // Implement your add function here. This should use
 // template to be able to take a generic number.
 // Should handle add(T first,T second) call.
 
 int main() {
+	Number * first;
+	Number * second;
 	first = new Number(BLUE);
 	second = new Number(GREEN);
 	cout << add(first, second) << endl;
